09d4c35b8d3a44acd394e2bde7a7fbd4d212a2a2
Lian <yichengxin7@gmail.com>
2026-01-21 14:31:30 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ def main():
     
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        host="127.0.0.1",
+        port=8722,
         reload=True,
         log_level="info"
     )
